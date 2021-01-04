284f1b63007cbed2541f0925705f31b1ea3c4902
basharinandrej <basharin_andrej@mail.ru>
2021-01-04 10:37:22 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// console.log('hello, World!')
+// let isSunk = false;
+// let hits = 0;
+// let gueses = 0;
+// let RandomLoc = Math.floor(Math.random()*5);
+// let location1 = RandomLoc;
+// let location2 = location1 + 1;
+// let location3 = location2 + 1;
+// let guese;
+// while(isSunk == false){
+//    guese = prompt('Введи число от 0 до 6');
+//       if(prompt < 0 || prompt > 6){
+//          alert('Эй, ты что дурак? Ты дурaк?');
+//       }else{
+//          gueses = gueses + 1; 
+//          if(guese == location1 || guese == location2 || guese == location3){
+//             alert('hit!')
+//             hits = hits + 1;
+//             if(hits == 3){
+//                isSunk = true;
+//                alert('fffuck!!!');
+//             }
+//          }else{
+//             alert('miss!');
+         
+//          }
+//       }
+
+// }
+// let statisic = 'eeeboyy, u sunk my ship. Твоя точность составила ' + hits/gueses + ' попугаев'
+// window.alert(statisic);
