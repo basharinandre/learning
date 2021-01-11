f38586f44aa43257f68af9c4d376baff27306e0d
elik <elik@example.com>
2021-01-11 21:44:12 +0300

    ScssCaseTesting

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider--first-js').slick();
+});
+
+$(document).ready(function(){
+    $('.slider--second-js').slick();
+});
+
+$(document).ready(function(){
+    $('.slider--third-js').slick();
+});
