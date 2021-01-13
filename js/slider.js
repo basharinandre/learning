29f744685c8d0ca5946201eb8c27c1bf5233b6f2
elik <elik@example.com>
2021-01-13 22:59:34 +0300

    SliderAndSection

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
-    $('.slider--first-js').slick();
+    $('.slider--first-js').slick({
+        dots: true,
+    });
 });
 
 $(document).ready(function(){
-    $('.slider--second-js').slick();
+    $('.slider--second-js').slick({
+        dots: true,
+    });
 });
 
 $(document).ready(function(){
-    $('.slider--third-js').slick();
+    $('.slider--third-js').slick({
+        dots: true,
+    });
 });
