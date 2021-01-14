d35d662a6d480c334c66e94d038f57aa479b046f
elik <elik@example.com>
2021-01-14 23:16:32 +0300

    firstProblems

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider--first-js').slick({
         dots: true,
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider--second-js').slick({
         dots: true,
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider--third-js').slick({
         dots: true,
     });
 });
+
+let question = 'Привет, как дела?'
+
+function Сontinue(answer){
+    if(answer === 'Хей, привет'){
+        alert('Классные котики у тебя, сильная и независимая)')
+    }else if(answer === 'Отвали'){
+        alert('(((, но котики всё-равно классные!')
+    }else(answer === 'Ух ты, так ещё никто не подкатывал)'){
+        alert('То ли ещё будет!')
+    }
+}
